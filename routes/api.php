d27012b85d953579bee1d63f0655bb9a3c6b6fb7
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-08-29 13:52:10 +0200

    [FEATURES] impostato rotta in api e response in controller api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+/* Imposto la rotta su posts, e passo il post controller in API come class e parametro index */
+Route::get('/posts', [PostController::class, 'index']);
