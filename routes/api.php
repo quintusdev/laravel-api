536a63975823b53db05e328d7779f7ff76092f9d
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-09-01 14:49:07 +0200

    [UPDATE] Aggiunto use in api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\LeadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\LeadController;
 
 /*
 |--------------------------------------------------------------------------
